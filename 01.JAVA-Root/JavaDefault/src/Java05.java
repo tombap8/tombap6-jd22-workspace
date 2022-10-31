819b84cf252bf6b9b8b5bbb1663ae6d4ffa0fc63
tombraceparker <tombap8@hotmail.com>
2022-10-31 23:08:59 +0900

    ss

--- 01.JAVA-Root/JavaDefault/src/Java05.java
+++ 01.JAVA-Root/JavaDefault/src/Java05.java
@@ -1,12 +1,23 @@
+import module1.*;
 public class Java05 {
     public static void main(String[] args) {
         myFn();
         System.out.println(myCalc(3, 1000));
+        Jmo1 jmo1= new Jmo1();
+        jmo1.jsubFn();
+        jmo1.special();
+
+        Java05 j05 = new Java05();
+        var print = j05.myStr(100, "명이야!");
+        System.out.println(print);
     }
     static void myFn(){
         System.out.println(333);
     }
     static int myCalc(int a, int b){
         return a * b;
+    }
+    private String myStr(int a, String b){
+        return a+b;
     }
 }
---
+++ 01.JAVA-Root/JavaDefault/src/module1/Jmo1.java
@@ -0,0 +1,13 @@
+package module1;
+
+public class Jmo1 {
+    public static void main(String[] args) {
+        System.out.println("Jmo1이야~!");
+    }
+    public void jsubFn(){
+        System.out.println("jsubFn함수양~!");
+    }
+    public void special(){
+        System.out.println("스페셜이양~!");
+    }
+}
