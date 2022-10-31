931cd11082e211b0096fcd607576cab4169a88be
Tom B. Parker <tombap8@hotmail.com>
2022-10-31 17:12:36 +0900

    ss

--- 01.JAVA-Root/JavaDefault/src/Java05.java
+++ 01.JAVA-Root/JavaDefault/src/Java05.java
@@ -1,8 +1,12 @@
 public class Java05 {
     public static void main(String[] args) {
         myFn();
+        System.out.println(myCalc(3, 1000));
     }
     static void myFn(){
         System.out.println(333);
+    }
+    static int myCalc(int a, int b){
+        return a * b;
     }
 }
