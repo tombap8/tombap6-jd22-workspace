4d35a7768eb3ed9b7b6dab63e61c18a30c1dfc3e
Tom B. Parker <tombap8@hotmail.com>
2022-11-24 13:48:11 +0900

    ss

--- 01.JAVA-Root/JavaDefault/src/Java08.java
+++ 01.JAVA-Root/JavaDefault/src/Java08.java
@@ -1,19 +1,19 @@
 public class Java08{
     public static void main(String[] args) {
         
-    }
-}
+    } /// main ///
+} /// class ///
 
 class Animal{
     protected String name = "동물";
     public String sound(String ss){
-        System.out.println("동물소리:"+ss);
+        return "동물소리:"+ss;
     }
 }
 
 class Pig extends Animal{
     private String weight = "80kg";
-    public static void main(String[] args) {
+    public void makePig() {
         Animal animal = new Animal();
 
         String original = animal.sound("꿀꿀");
